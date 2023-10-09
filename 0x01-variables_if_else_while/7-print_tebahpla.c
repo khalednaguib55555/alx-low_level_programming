c88dc87ef190351eb1066335d8ab7f28f0f1dba5
khaled <knaguib55555@gmail.com>
2023-10-09 03:29:06 -0700

    fd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: 'good program this'
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch > 'a')
+	{
+		putchar (ch);
+		--ch;
+	}
+	putchar ('\n');
+
+	return (0);
+}
+
