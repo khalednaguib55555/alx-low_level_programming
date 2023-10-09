3301722cd7788e3e212aefcd10830a51195d01c8
khaled <knaguib55555@gmail.com>
2023-10-09 01:41:14 -0700

    sdf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Description: 'good program this'
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
