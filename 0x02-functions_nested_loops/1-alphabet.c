2f77c2132f6b88448de3b76f83cab8996adf6524
khaled <knaguib55555@gmail.com>
2023-10-10 05:11:38 -0700

    sa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * main - entry
+ *
+ * Description: 'good program this'
+ *
+ * Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+
