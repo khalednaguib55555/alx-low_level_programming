21927e61aaf0661ca9b8afa375a078324eedd5aa
khaled <knaguib55555@gmail.com>
2023-10-12 04:11:05 -0700

    ds

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - check for letter
+ *
+ * @c: is the character
+ *
+ * Return: 1 if c is uper, 0 if not
+*/
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
